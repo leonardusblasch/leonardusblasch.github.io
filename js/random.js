d9ac3b88278a61abfbe20f70314fb7a7d5a13537
leonardusblasch <leontsurbala@gmail.com>
2022-02-03 01:35:29 +0100

    Added 'impressum'

---
+++ js/random.js
@@ -0,0 +1,27 @@
+randomBib();
+
+document.getElementById('reload').onclick = function()
+{
+    randomBib();
+}
+
+function randomBib()
+{
+    let link = document.getElementById('random');
+    link.innerHTML = '';
+
+    let rbook = randomInt(bibel.length);
+    let rchap = randomInt(bibel[rbook].length);
+    let rvers = randomInt(bibel[rbook][rchap].length);   
+
+    let rinfo = books[rbook] + ' ' + (rchap + 1) + ', ' + (rvers + 1);
+    let rbibel = bibel[rbook][rchap][rvers];
+
+    link.href = '../bibel/index.html';
+    link.innerHTML = '<u>' + rinfo + '</u>' + '<br>' + rbibel;
+}
+
+function randomInt(max)
+{
+    return Math.floor(Math.random() * max);
+}
