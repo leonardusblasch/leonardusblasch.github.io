f253f1b7c6efa1cd56abdd3cb1f00bc8ac578879
leonardusblasch <leontsurbala@gmail.com>
2022-02-04 20:43:09 +0100

    Added Davee

---
+++ js/settings.js
@@ -0,0 +1,34 @@
+let r, g, b;
+
+let settings = function (p)
+{
+	var myDiv = document.getElementById("settings");
+	let width = myDiv.clientWidth;
+	let height = myDiv.clientHeight;
+
+
+	p.setup = function ()
+	{
+		p.noCanvas();
+		r = new Slider(0, 255, 100, '10p)
+		g = p.createSlider(0, 255, 215);
+		b = p.createSlider(0, 255, 0);
+	};
+
+	class Slider
+	{
+		constructor(min, max, value, x, y, w, h)
+		{
+			this.slider = p.createSlider(min, max, value);
+			p.slider.position(x, y);
+			p.slider.style('width', w);
+			p.slider.style('height', h);
+		}
+
+		value()
+		{
+			return this.slider.value();
+		}
+	}
+};
+new p5(settings, 'settings');
