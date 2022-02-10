ebb231d5ede22c264f55d2f9b548cc56f4610b65
leonardusblasch <leontsurbala@gmail.com>
2022-02-10 16:00:12 +0100

    Added PeriodicWave

--- js/music.js
+++ js/music.js
@@ -16,10 +16,9 @@ let music = function (p)
 	let len = 1;
 	let prev;
 
+
 	p.setup = function ()
 	{
-		rev = new p5.Delay();
-
 		p.pixelDensity(1);
 
 		prev = new p5.MonoSynth();
@@ -52,7 +51,7 @@ let music = function (p)
 		p.image(grid.graph, 0, 0);
 		p.image(grid.notes, 0, 0);
 
-		if(reload)
+		if (reload)
 		{
 			if (tone != toneS.value())
 			{
@@ -82,6 +81,7 @@ let music = function (p)
 			}
 
 			tact = 60.0 / (tempo * grid.parts);
+			player = new Player();
 			reload = false;
 		}
 
@@ -100,7 +100,7 @@ let music = function (p)
 		else
 		{
 			player.line();
-        }
+		}
 	};
 
 	p.windowResized = function ()
@@ -115,7 +115,7 @@ let music = function (p)
 		{
 			grid.touch(p.mouseX, p.mouseY);
 		}
-    }
+	}
 
 	function resize()
 	{
@@ -136,7 +136,7 @@ let music = function (p)
 	{
 		constructor()
 		{
-			this.synth = new p5.PolySynth(p5.MonoSynth, 7 * 8);
+			this.synth = new p5.PolySynth(PeriodicWave, 7 * 8);
 			this.zero = p.millis();
 			this.count = -1;
 			this.off = 0;
@@ -145,7 +145,7 @@ let music = function (p)
 		line()
 		{
 			p.line(this.off, 0, this.off, p.height);
-        }
+		}
 
 		play()
 		{
@@ -179,9 +179,9 @@ let music = function (p)
 				}
 
 				this.zero = p.millis();
-            }
+			}
 		}
-    }
+	}
 
 	class Grid
 	{
@@ -216,7 +216,7 @@ let music = function (p)
 				let y = i * sizeH;
 				this.graph.line(0, y, p.width, y);
 			}
-			
+
 			let x = 0;
 			for (let bar = 0; bar < this.bars; bar++)
 			{
@@ -250,9 +250,9 @@ let music = function (p)
 				if (n.bar >= this.bars || n.beat >= this.beats || n.part >= this.parts || n.octa < this.min || n.octa > this.max)
 				{
 					this.array.splice(i, 1);
-                }
-            }
-        }
+				}
+			}
+		}
 
 		setup()
 		{
@@ -307,7 +307,197 @@ let music = function (p)
 			this.part = part;
 			this.octa = octa;
 			this.note = note;
-        }
+		}
 	}
+
+	function getEnv()
+	{
+		let half = tact / 2;
+		return new p5.Envelope(half, 1, tact, 0)
+    }
+
+	function PeriodicWave(params)
+	{
+		p5.AudioVoice.call(this);
+		this.osc = new p5.Oscillator('sine');
+
+		this.real = new Float32Array([0, 0]);
+		this.imag = new Float32Array([0, 1]);
+		this.wt = this.ac.createPeriodicWave(this.real, this.imag);
+
+		this.osc.disconnect();
+		this.osc.start();
+
+		//this.env = new p5.Envelope(0.021, 0.025, 0.025, 0.025, 0.95, 0.33, 0.25);
+
+		this.env = getEnv();
+		this.env.disconnect();
+
+		this.filter = new p5.LowPass();
+		this.filter.set(22050, 5);
+
+
+		this.env.connect(this.filter);
+		this.osc.connect(this.filter);
+
+		this.connect();
+
+		this.filter.set(22050, 5);
+
+
+		this.setParams = function (params)
+		{
+			this.real = new Float32Array(params.real);
+			this.imag = new Float32Array(params.imag);
+			this.wt = this.ac.createPeriodicWave(this.real, this.imag);
+			this.osc.oscillator.setPeriodicWave(this.wt);
+		}
+
+		this.setADSR = function (aTime, aLevel, dTime, dLevel)
+		{
+			this.env.set(aTime, aLevel, dTime, dLevel)
+		}
+
+		this.play = function ()
+		{
+			this.env.play(this.filter);
+
+		}
+
+		this.triggerAttack = function (note, velocity, secondsFromNow)
+		{
+			var secondsFromNow = secondsFromNow || 0;
+
+			//triggerAttack uses ._setNote to convert a midi string to a frequency if necessary
+			var freq = typeof note === 'string' ? this._setNote(note)
+				: typeof note === 'number' ? note : 440;
+			var vel = velocity || 1;
+			// this.env.setRange(this.env.aLevel / velocity,0);
+			this._isOn = true;
+			this.osc.freq(freq, 0, secondsFromNow);
+			this.env.triggerAttack(this.filter, secondsFromNow);
+
+		}
+
+		this.triggerRelease = function (secondsFromNow)
+		{
+			var secondsFromNow = secondsFromNow || 0;
+			this.env.triggerRelease(this.filter, secondsFromNow);
+			this._isOn = false;
+		}
+
+	}
+
+	PeriodicWave.prototype = new p5.AudioVoice();
+	PeriodicWave.prototype.constructor = PeriodicWave;
+
+
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	// A typical synth class which inherits from AudioVoice class
+	function SquareVoice()
+	{
+
+		p5.MonoSynth.call(this);
+
+		this.oscillator.setType('square');
+	}
+	SquareVoice.prototype = Object.create(p5.MonoSynth.prototype);  // browsers support ECMAScript 5 ! warning for compatibility with older browsers
+	SquareVoice.prototype.constructor = SquareVoice;
+
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	// A Detuned synth
+	function DetunedOsc()
+	{
+
+		p5.MonoSynth.call(this);
+
+		this.osctype = 'sine';
+		this.detune = -5;
+
+		this.oscOne = this.oscillator;
+		this.oscTwo = new p5.Oscillator();
+
+		this.filter.setType('lowpass');
+		this.filter.set(22050, 5);
+
+		this.oscOne.disconnect();
+		this.oscTwo.disconnect();
+
+
+		this.oscOne.connect(this.filter);
+		this.oscTwo.connect(this.filter);
+
+
+		this.env.setInput(this.oscOne, this.oscTwo);
+
+		this.oscOne.start();
+		this.oscTwo.start();
+
+
+		this.triggerAttack = function (note, velocity, secondsFromNow)
+		{
+			this.oscTwo.oscillator.detune.value
+			var secondsFromNow = secondsFromNow || 0;
+			var freq = typeof note === 'string' ? this._setNote(note)
+				: typeof note === 'number' ? note : 440;
+			var vel = velocity || 1;
+
+			this._isOn = true;
+
+			this.oscOne.freq(freq, 0, secondsFromNow);
+			this.oscTwo.freq(freq + this.detune, 0, secondsFromNow);
+			this.env.ramp(this.output, secondsFromNow, this.env.aLevel);
+		}
+
+	}
+
+	DetunedOsc.prototype = Object.create(p5.MonoSynth.prototype);
+	DetunedOsc.prototype.constructor = DetunedOsc;
+
+
+
+	function AdditiveSynth()
+	{
+		p5.MonoSynth.call(this);
+
+		this.osctype = 'triangle';
+		this.harmonics = [1, 2, 4, 6, 8];
+		this.note = 60;
+
+		this.oscbank = [];
+		this.oscillator.dispose();
+		delete this.oscillator;
+		this.env.disconnect();
+
+		for (var i = 0; i < this.harmonics.length; i++)
+		{
+			this.oscbank.push(new p5.Oscillator());
+			this.oscbank[i].setType(this.osctype);
+			this.oscbank[i].disconnect();
+			this.oscbank[i].connect(this.filter);
+			this.env.connect(this.oscbank[i]);
+			this.oscbank[i].start();
+		}
+
+		this.triggerAttack = function (note, velocity, secondsFromNow)
+		{
+			var secondsFromNow = secondsFromNow || 0;
+			var freq = typeof note === 'string' ? this._setNote(note)
+				: typeof note === 'number' ? note : 440;
+			var vel = velocity || 1;
+
+			this._isOn = true;
+
+			for (var i = 0; i < this.harmonics.length; i++)
+			{
+				this.oscbank[i].freq(freq + midiToFreq(this.harmonics[i] * 12), 0, secondsFromNow);
+			}
+
+			this.env.ramp(this.output, secondsFromNow, this.env.aLevel);
+		}
+
+	}
+	AdditiveSynth.prototype = Object.create(p5.MonoSynth.prototype);
+	AdditiveSynth.prototype.constructor = AdditiveSynth;
 };
 new p5(music, 'music');
