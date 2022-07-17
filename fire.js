864888e98c41054ffd8a0cb07ba635703f4552cf
leonardusblasch <98529686+leonardusblasch@users.noreply.github.com>
2022-07-17 16:12:20 +0200

    Firebase

---
+++ fire.js
@@ -0,0 +1,28 @@
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-app.js";
+import { getDatabase, ref, get, child } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-database.js";
+
+const config = {
+    apiKey: "AIzaSyA6kQCBWrMfhOU0UrTQl4FGfgDjf3S3QiI",
+    authDomain: "bibel7.firebaseapp.com",
+    databaseURL: "https://bibel7-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "bibel7",
+    storageBucket: "bibel7.appspot.com",
+    messagingSenderId: "361153938395",
+    appId: "1:361153938395:web:635cfdb14ba53be850ddfe"
+  };
+
+const app = initializeApp(config);
+const db = getDatabase(app);
+const r = ref(db)
+
+get(child(r, "bible/0/0/0")).then((snapshot) => {
+    if (snapshot.exists()) {
+      console.log(snapshot.val());
+    } else {
+      console.log("no data");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
