d93bed07c186edc278e71de7441cc314748b5b0d
leontsurbala@gmail.com <leontsurbala@gmail.com>
2022-07-17 20:21:49 +0200

    FB

--- fire.js
+++ fire.js
@@ -1,28 +1,48 @@
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-app.js";
 import { getDatabase, ref, get, child } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-database.js";
 
 const config = {
-    apiKey: "AIzaSyA6kQCBWrMfhOU0UrTQl4FGfgDjf3S3QiI",
-    authDomain: "bibel7.firebaseapp.com",
-    databaseURL: "https://bibel7-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "bibel7",
-    storageBucket: "bibel7.appspot.com",
-    messagingSenderId: "361153938395",
-    appId: "1:361153938395:web:635cfdb14ba53be850ddfe"
-  };
+  apiKey: "AIzaSyA6kQCBWrMfhOU0UrTQl4FGfgDjf3S3QiI",
+  authDomain: "bibel7.firebaseapp.com",
+  databaseURL: "https://bibel7-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "bibel7",
+  storageBucket: "bibel7.appspot.com",
+  messagingSenderId: "361153938395",
+  appId: "1:361153938395:web:635cfdb14ba53be850ddfe"
+};
 
 const app = initializeApp(config);
 const db = getDatabase(app);
-const r = ref(db)
+const r = ref(db);
 
-get(child(r, "bible/0/0/0")).then((snapshot) => {
-    if (snapshot.exists()) {
-      console.log(snapshot.val());
-    } else {
-      console.log("no data");
-    }
-  }).catch((error) => {
-    console.error(error);
-  });
+export class fb
+{
+  static title(index) {
+    return this.path(`title/${index}`);
+  }
+  
+  static chaps(book) {
+    return this.path(`chaps/${book}`);
+  }
+  
+  static count(b, c) {
+    return this.path(`count/${b}/${c}`);
+  }
+  
+  static bible(b, c, v) {
+    return this.path(`bible/${b}/${c}/${v}`);
+  }
+  
+  static path(path) {
+    return get(child(r, path)).then((snapshot) => {
+      if (snapshot.exists()) {
+        return snapshot.val();
+      } else {
+        return "no data";
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
+}
---
+++ script.js
@@ -0,0 +1,19 @@
+import { fb } from "./fire.js";
+
+let book = Math.floor(Math.random()*66);
+
+
+
+/*
+let title = await fb.title(0);
+console.log(title);
+
+let chaps = await fb.chaps(0);
+console.log(chaps);
+
+let count = await fb.count(0, 0);
+console.log(count);
+
+let verse = await fb.bible(0, 0, 0);
+console.log(verse);
+*/
