bc352068c95ec96d92ac22c92a35fefcffe55976
lab <>
2023-09-27 17:09:20 +0200

    creds

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+console.log("created with p5.js @ https://p5js.org/copyright.html");
+
 let z = 0.0;
 let perlin;
 
@@ -16,4 +18,25 @@ function draw() {
 function initSketch() {
     createCanvas(windowWidth, windowHeight);
     perlin = new Perlin();
+}
+
+class Perlin {
+    constructor() {
+        noStroke();
+        colorMode(HSB, 1);
+        noiseSeed(42)
+        this.size = ceil(max(width, height) * 0.01);
+        this.cols = ceil(width / this.size);
+        this.rows = ceil(height / this.size);
+    }
+    draw() {
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.cols; x++) {
+                let nx = x * 0.005;
+                let ny = y * 0.005;
+                fill(noise(nx, ny, z), 50, 100);
+                rect(x * this.size, y * this.size, this.size);
+            }
+        }
+    }
 }
